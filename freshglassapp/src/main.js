1d3aca92632423e6203664058ed349f7f69aa4f6
Michael <mlebow@gmail.com>
2015-04-10 12:46:14 -0700

    Device discover setup

--- freshglassapp/src/main.js
+++ freshglassapp/src/main.js
@@ -1,3 +1,16 @@
 //@program
 
-application.add(new Container());
+
+//This does device discovery
+var ApplicationBehavior = Behavior.template({
+	onDisplayed: function(application) {
+		application.discover("freshglassdevice");
+	},
+	onQuit: function(application) {
+		application.forget("freshglassdevice");
+	},
+})
+
+
+application.behavior = new ApplicationBehavior();
+application.add(new Container());  //Was automatically generated with the new file
