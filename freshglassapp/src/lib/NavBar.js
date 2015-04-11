b6f9ba6979bec2edeadf43ebd415996d0aeae646
Noah Gilmore <noah.w.gilmore@gmail.com>
2015-04-11 12:24:19 -0700

    purely aesthetic NavBar.js changes because im OCD about javascript

--- freshglassapp/src/lib/NavBar.js
+++ freshglassapp/src/lib/NavBar.js
@@ -1,11 +1,9 @@
 // KPR Script file
 var THEME = require('themes/flat/theme');
 THEME.buttonStyle = new Style({ font: "20px Georgia"});
-THEME.buttonSkin = new Skin({fill: "#00FFFFFF", borders:{left: 2, right:2, bottom:2}, stroke:"black" })
-
+THEME.buttonSkin = new Skin({fill: "#00FFFFFF", borders:{left: 2, right:2, bottom:2}, stroke:"black" });
 
 var BUTTONS = require('controls/buttons');
-
 
 var backSkin = new Skin({fill:"transparent"});
 var navBarSkin = new Skin({fill:"#774A8E"});
@@ -15,33 +13,34 @@ var navBarHeight = 35;
 var windowNameStyle = new Style({font:"bold 25px", color:"white"});
 var backStyle = new Style({font:"15", color:"white"});
 
-
-var backButtonTemplate = BUTTONS.Button.template(function($){ return {
+var BackButtonTemplate = BUTTONS.Button.template(function($){ return {
 	left:0, bottom:0, width:60, height:navBarHeight, skin: backSkin,
 	contents: [
-		new Label({left:0, right:0, height:navBarHeight, string:"< Back", style: backStyle})
+		new Label({left: 0, right: 0, height: navBarHeight, string: "< Back", style: backStyle})
 	],
 	behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
-		onTap: { value: function(content){
+		onTap: { value: function(content) {
 			$.page.switchPages($.page.previousPage);
 		}}
 	})
 };});
 
-var windowNameTemplate = Label.template(function($) { return {
-	left: 0, right: 0, bottom: 0, height: navBarHeight, string:$.name, style: windowNameStyle
+var WindowNameTemplate = Label.template(function($) { return {
+	left: 0, right: 0, bottom: 0, height: navBarHeight, string: $.name, style: windowNameStyle
 };});
 
-var navBar = Line.template(function($) { 
+var NavBar = Line.template(function($) {
 	var buttonTemp = null;
 	if ($.back) {
-		var buttonTemp = new backButtonTemplate({page:$.page});
+		buttonTemp = new BackButtonTemplate({page: $.page});
 	}
 	return {
-	left:0, right:0, top:0, height: navBarHeight, skin: navBarSkin, contents: [
-		buttonTemp,
-		new windowNameTemplate({name: $.name }),
-	]};}
-);
+		left:0, right:0, top:0, height: navBarHeight, skin: navBarSkin,
+		contents: [
+			buttonTemp,
+			new WindowNameTemplate({name: $.name }),
+		]
+	};
+});
 
-module.exports = navBar;
+module.exports = NavBar;
