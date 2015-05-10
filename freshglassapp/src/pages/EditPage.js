147cca0efa5511a5bd33cdee1c21cdf09dcc71fd
Noah Gilmore <noah.w.gilmore@gmail.com>
2015-05-10 14:22:02 -0700

    cleanup for edit page. will do windowSelector next

--- freshglassapp/src/pages/EditPage.js
+++ freshglassapp/src/pages/EditPage.js
@@ -1,21 +1,67 @@
 //@module
+var SLIDERS = require('controls/sliders');
+
 var NavBar = require("lib/NavBar");
 var WindowSelector = require("lib/WindowSelector");
-
 var CameraRoll = require("pages/CameraRoll");
-var SLIDERS = require('controls/sliders');
 var Window = require('lib/Window');
 
+// definitions of styles, common skins, etc
+var red = "#db3a1c";
+var blue = "#4682EA";
+var yellow = "#FDBA35";
+var orange = "#FF7F00";
+var green = "#67AF4B";
+var purple = "#AF6DC5";
+var darkBlue = "#43489B";
+
+var applyButtonSkin = new Skin({fill: green, stroke: "black"});
+var cancelButtonSkin = new Skin({fill: orange, stroke: "black"});
+var clearButtonSkin = new Skin({fill: blue, stroke: "black"});
+var undoButtonSkin = new Skin({fill: blue, stroke: "white"});
+
+var controlURI = mergeURI(application.url, "images/controlbutton.png");
+var controlWithLabelURI = mergeURI(application.url, "images/controlbuttonwithlabel.png");
+
+var imagesSkin = new Skin({fill: blue, borders: {bottom: 2, right: 1}, stroke: "gray"});
+var controlSkin = new Skin({fill: green, borders: {bottom: 2}, stroke: "gray"});
+
+var rootContainterSkin = new Skin({fill: "white"});
+var tintContainerSkin = new Skin({fill: "white"});
+var imagesContainerSkin = new Skin({fill: "white"});
+var controlContainerSkin = new Skin({fill: "white"});
+
+var tintSelectedSkin = new Skin({fill: "white", borders: {right: 2, top: 2}, stroke:"gray"});
+var imagesSelectedSkin = new Skin({fill: "white", borders: {left: 1, right: 2, top: 2}, stroke:"gray"});
+var controlSelectedSkin = new Skin({fill: "white", borders: {left: 1, top: 2}, stroke:"gray"});
+
+var unselectedTintSkin = new Skin({fill: "white", borders:{bottom:2, top: 1, right: 1}, stroke:"gray"});
+var unselectedImagesSkin = new Skin({fill: "white", borders:{bottom:2, top: 1, right: 1}, stroke:"gray"});
+var unselectedControlSkin = new Skin({fill: "white", borders:{bottom:2, top: 1}, stroke:"gray"});
+
+var unselectedStyle = new Style({color: "gray", font: "20px Lucinda Grande"});
+var controlLabelStyle = new Style({color: "gray", font: "14px Lucinda Grande"});
+
+var selectedStyle = new Style({color: blue, font: "bold 20px Lucinda Grande"});
+
+var tintRightBorderSkin = new Skin({fill: "white", borders: {right: 1, bottom: 2, top: 1}, stroke:"gray"});
+var imagesRightBorderSkin = new Skin({fill: "white", borders: {right: 1, bottom: 2, top: 1}, stroke:"gray"});
+var controlRightBorderSkin = new Skin({fill: "white", borders: {right: 1, bottom: 2, top: 1}, stroke:"gray"});
+
+var addImageSkin = new Skin({fill:blue});
+
+/**
+ * Actual edit page for a given window.
+ */
 var EditPage = function (window, switchPages) {
     this.window = window;
     this.window.editPage = this;
 
     this.name = "edit";
-    //this.previousPage = previousPage;
     this.switchPages = switchPages;
     
     this.lastAction = "";
-    
+
     this.container = null;
     this.windowPreviewContainer = null;
     this.currentTab = "tint";
@@ -44,13 +90,20 @@ var EditPage = function (window, switchPages) {
 
     this.undoAutoLine = null;
     this.tintCheckbox = null;
-    //this.windowCopy = this.window;
 };
 
+/**
+ * The main window for an edit page, as far as the nav bar is concerned is
+ * the window you can edit on it.
+ */
 EditPage.prototype.getMainWindow = function () {
     return this.window;
 };
 
+/**
+ * When the edit page is navigated to, be sure to put the window preview into its
+ * root container.
+ */
 EditPage.prototype.onNavigatedTo = function () {
     // if we already rendered the kinoma structure for this page, make sure that we
     // still have the window preview here, because it may have been removed to get
@@ -61,57 +114,10 @@ EditPage.prototype.onNavigatedTo = function () {
     }
 };
 
-var red = "#db3a1c";
-var blue = "#4682EA";
-var yellow = "#FDBA35";
-var orange = "#FF7F00";
-var green = "#67AF4B";
-var purple = "#AF6DC5";
-var darkBlue = "#43489B";
-
-var applyButtonSkin = new Skin({fill: green, stroke:"black"});
-var cancelButtonSkin = new Skin({fill: orange, stroke:"black"});
-var clearButtonSkin = new Skin({fill: blue, stroke:"black"});
-var undoButtonSkin = new Skin({fill:blue , stroke:"white"});
-
-var tran = new Skin({fill: "white"});
-
-var controluri = mergeURI(application.url, "images/controlbutton.png");
-var controlwithlabeluri = mergeURI(application.url, "images/controlbuttonwithlabel.png");
-
-var lefturi = mergeURI(application.url, "images/leftbutton.png");
-var righturi = mergeURI(application.url, "images/rightbutton.png");
-var upuri = mergeURI(application.url, "images/upbutton.png");
-var downuri = mergeURI(application.url, "images/downbutton.png");
-
-var imagesSkin = new Skin({fill: blue, borders:{bottom:2, right:1}, stroke: "gray"});
-var controlSkin = new Skin({fill: green, borders:{bottom:2}, stroke:"gray"});
-
-var rootContainterSkin = new Skin({fill: "white"});
-var tintContainerSkin = new Skin({fill: "white"});
-var imagesContainerSkin = new Skin({fill: "white"});
-var controlContainerSkin = new Skin({fill: "white"});
-
-var tintSelectedSkin = new Skin({fill: "white", borders:{right:2, top:2}, stroke:"gray"});
-var imagesSelectedSkin = new Skin({fill: "white", borders:{left:1, right:2, top:2}, stroke:"gray"});
-var controlSelectedSkin = new Skin({fill: "white", borders:{left:1, top:2}, stroke:"gray"});
-
-var unselectedTintSkin = new Skin({fill: "white", borders:{bottom:2, top: 1, right: 1}, stroke:"gray"});
-var unselectedImagesSkin = new Skin({fill: "white", borders:{bottom:2, top: 1, right: 1}, stroke:"gray"});
-var unselectedControlSkin = new Skin({fill: "white", borders:{bottom:2, top: 1}, stroke:"gray"});
-
-var unselectedStyle = new Style({color: "gray", font: "20px Lucinda Grande"});
-var controlLabelStyle = new Style({color: "gray", font: "14px Lucinda Grande"});
-
-var selectedStyle = new Style({color: blue, font: "bold 20px Lucinda Grande"});
-
-var tintRightBorderSkin = new Skin({fill: "white", borders:{right:1, bottom: 2, top: 1}, stroke:"gray"});
-var imagesRightBorderSkin = new Skin({fill: "white", borders:{right:1, bottom: 2, top: 1}, stroke:"gray"});
-var controlRightBorderSkin = new Skin({fill: "white", borders:{right:1, bottom: 2, top: 1}, stroke:"gray"});
-
-var addImageSkin = new Skin({fill:blue});
-
-    
+/**
+ * Given a tab string that is either "control", "images", or "tint",
+ * select that given tab by making it larger/highlighted in the kinoma container.
+ */
 EditPage.prototype.activateTab = function (tab) {
 
     this.tabContainers["control"].skin = unselectedControlSkin;
@@ -134,20 +140,17 @@ EditPage.prototype.activateTab = function (tab) {
         this.tabContainers["images"].skin = unselectedImagesSkin;
         this.container.skin = tintContainerSkin;
         this.controlContainer.skin = tintContainerSkin;
-        
     } else if (this.currentTab == "images") {
         this.tabContainers[this.currentTab].skin = imagesSelectedSkin;
         this.tabContainers[this.currentTab].first.style = selectedStyle;
         this.container.skin = imagesContainerSkin;
         this.controlContainer.skin = imagesContainerSkin;
-        
     } else if (this.currentTab == "control"){
         this.tabContainers[this.currentTab].skin = controlSelectedSkin;
         this.tabContainers[this.currentTab].first.style = selectedStyle;
         this.tabContainers["tint"].skin = unselectedTintSkin;
         this.container.skin = controlContainerSkin;
         this.controlContainer.skin = controlContainerSkin;
-        
     }
 };
 
@@ -160,7 +163,10 @@ EditPage.prototype.getContainer = function () {
     var page = this;
 
     var navBar = new NavBar({ page: page });
-    var windowSelector = new WindowSelector({edit: true, presets: false, status: false, page: page, name: page.window.name });
+    var windowSelector = new WindowSelector({
+        edit: true, presets: false, status: false,
+        page: page, name: page.window.name
+    });
 
     var TintTab = BUTTONS.Button.template(function ($) { return {
         left: 0, right: 0, top: 0, bottom: 0,
@@ -252,29 +258,26 @@ EditPage.prototype.getContainer = function () {
             }),
         ]
     };});
-      
+
     var controlButton = BUTTONS.Button.template(function ($) { return {
         left: 15, right: 0, top: 0, height: 70,
         skin: controlContainerSkin,
         behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
             onTap: { value: function (button) {
-                if (page.controlID !== null) {
-                    // do nothing
-                } else {
-                    page.controlID = page.window.addImage(controluri, 130, 130, 25, 25);
-                    //trace(page.controlID + "\n");
+                if (page.controlID === null) {
+                    page.controlID = page.window.addImage(controlURI, 130, 130, 25, 25);
                     page.window.clearImages = false;
                 }
             }}
         }),
         contents: [
-            new Picture({left: 0, top:0, width: 100, height:50, url: controlwithlabeluri}),
+            new Picture({left: 0, top:0, width: 100, height:50, url: controlWithLabelURI}),
         ]
     };});
 
     page.controls.tint = new TintSlider();
-    page.controls.images = new AddImageButton(page); //TODO: change!
-    page.controls.control = new controlButton(); //TODO: change!
+    page.controls.images = new AddImageButton(page);
+    page.controls.control = new controlButton();
 
     page.controlContainer = new Container({
         left: 0, right: 0, height: 50,
@@ -283,55 +286,6 @@ EditPage.prototype.getContainer = function () {
             page.controls.tint
         ],
     });
-
-
-    // var ApplyButton = BUTTONS.Button.template(function ($) { return {
-    //     left: 10, right: 5, top: 0, height: 35,
-    //     skin: applyButtonSkin,
-    //     behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
-    //         onTap: { value: function (button) {
-    //             page.window.updateFrom(page.window);
-    //             page.window.updatePreview();
-    //         }}
-    //     }),
-    //     contents: [
-    //         new Label({
-    //             left: 0, right: 0, bottom: 0, top: 0,
-    //             style: new Style({color: "white", font: "Helvetica Neue", size: 18}),
-    //             string: "Apply"
-    //         })
-    //     ]
-    // };});
-
-    // var CancelButton = BUTTONS.Button.template(function ($) { return {
-    //     left: 5, right: 10, top: 0, height: 35,
-    //     skin: cancelButtonSkin,
-    //     behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
-    //         onTap: { value: function (button) {
-    //             trace(page.lastAction)
-    //             if (page.lastAction == "tint"){
-    //                 page.controls.tint.behavior.data.value = page.window.tint;
-    //                 page.controls.tint.behavior.onValueChanged();
-    //                 page.controls.tint.behavior.onLayoutChanged(page.controls.tint);
-    //             } else if (page.lastAction == "control"){                
-    //                 page.window.images = [];
-    //                 page.window.clearImages = true;
-    //                 //page.window.control.added = false;
-    //             } else if (page.lastAction == "images"){
-    //                 page.window.images.pop(page.window.images.length - 1);
-    //             }
-    //             page.window.updatePreview();
-    //         }}
-    //     }),
-    //     contents: [
-    //         new Label({
-    //             left: 0, right: 0, bottom: 0, top: 0,
-    //             style: new Style({color: "white", font: "Helvetica Neue", size: 18}),
-    //             string: "Cancel"
-    //         })
-    //     ]
-    // };});
-
 
     var ClearButton = BUTTONS.Button.template(function ($) { return {
         right: 15, height: 30, width: 70,
@@ -342,7 +296,6 @@ EditPage.prototype.getContainer = function () {
                 page.window.tint = 0;
                 page.window.images = [];
                 page.window.controls = null;
-                //page.window.control.added = false;
                 page.controls.tint.behavior.onValueChanged();
                 page.controls.tint.behavior.onLayoutChanged(page.controls.tint);
                 page.controlID = null;
@@ -372,7 +325,6 @@ EditPage.prototype.getContainer = function () {
                 } else if (page.lastAction == "control") {
                     page.window.images = [];
                     page.window.clearImages = true;
-                    //page.window.control.added = false;
                 } else if (page.lastAction == "images"){
                     page.window.images.pop(page.window.images.length - 1);
                 }
@@ -411,8 +363,7 @@ EditPage.prototype.getContainer = function () {
                 page.window.autoTint = false;
             }}
         })
-    }});
-
+    };});
 
     Handler.bind("/updateFirst", Behavior({
         onInvoke: function(handler, message){
@@ -421,7 +372,7 @@ EditPage.prototype.getContainer = function () {
                 page.controls.tint.behavior.data.value = page.window.tint;
                 page.controls.tint.behavior.onLayoutChanged(page.controls.tint);
                 handler.invoke(new Message("/updateSecond"));
-            }  
+            }
         },
     }));
 
@@ -443,7 +394,7 @@ EditPage.prototype.getContainer = function () {
                 style: new Style({color: "Black", size: 18, font: "Helvetica Neue"}),
                 string: "Auto-Tint"
             })
-        ]    
+        ]
     });
 
     page.tintCheckbox = autoTintCheckboxContainer;
@@ -455,7 +406,7 @@ EditPage.prototype.getContainer = function () {
             new ClearButton(),
             new UndoButton(),
         ]
-        });
+    });
 
     var rootColumn = new Column({
         top: 0, left: 0, bottom: 0, right: 0,
@@ -477,10 +428,10 @@ EditPage.prototype.getContainer = function () {
         ]
     });
 
-    rootContainter = new Container({
-        top:0, bottom: 0, left: 0, right: 0, 
+    var rootContainter = new Container({
+        top:0, bottom: 0, left: 0, right: 0,
         skin: rootContainterSkin,
-        contents: [ 
+        contents: [
             rootColumn
         ]
     });
